c0e49d88f280b3da9c8b67eac230f3fe722d87be
_ayaanfarhan <ayaan.phantom@gmail.com>
2026-07-21 15:38:59 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+//To start off, the form built in html needs to be linked to .js
+
+ const form= document.querySelector("#careerForm")
+
+ formInteract.addEventListener("submit", function(event) {
+  event.preventDefault();
+});
