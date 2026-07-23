ca1a5cdbf51257f829f987080eb19404c837ca55
_ayaanfarhan <ayaan.phantom@gmail.com>
2026-07-23 09:47:50 -0500

    Added JavaScript form handling and result card
    - updated files - 23rd JUly 2026

--- script.js
+++ script.js
@@ -2,6 +2,35 @@
 
  const form= document.querySelector("#careerForm")
 
- formInteract.addEventListener("submit", function(event) {
+ form.addEventListener("submit", function(event) {
   event.preventDefault();
+
+  const nameInput= document.querySelector("#name")
+  const name= nameInput.value
+  const majorInput= document.querySelector("#major")
+  const major= majorInput.value
+  const gradYearInput= document.querySelector("#gradYear")
+  const gradYear= gradYearInput.value
+  const skillsInput= document.querySelector("#skills")
+  const skills= skillsInput.value
+  const jobDescriptionInput= document.querySelector("#jobDescription")
+  const jobDescription= jobDescriptionInput.value
+
+  console.log(name);
+  console.log(major);
+  console.log(gradYear);
+  console.log(skills);
+  console.log(jobDescription);
+
+  const results= document.querySelector("#results")
+  results.innerHTML= `
+  <div class="results-card">
+   <p>Name: ${name}</p>
+   <p>Major: ${major}</p>
+   <p>Graduation Year: ${gradYear}</p>
+   <p>Skills: ${skills}</p>
+   <p>Job Description: ${jobDescription}</p>
+  </div>
+  `;
+  form.reset()
 });
